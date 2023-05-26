f66e99767476e26015c3834ccfe7b2a1c4866009
mvpea <pearceav@dukes.jmu.edu>
2023-05-25 20:53:19 -0400

    Final product

--- send-to-webhook/automated_notifications.sh
+++ send-to-webhook/automated_notifications.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # Curl command to slack and teams tutorial
 
-WEBHOOK_URL="https://hooks.slack.com/services/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"
+WEBHOOK_URL=https://hooks.slack.com/services/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
 payload="This is important... I found the hidden treasure!!!"
 
 # Loop through files and run things/ look for stuff
@@ -11,18 +11,18 @@ echo Doing stuff
 echo "I found something... I'll notify the team now."
 
 # Notify the Slack!!
-curl -X POST --data-urlencode "payload={\"channel\": \"#test-notifications\", \"username\": \"Happy Treasure Hunters\", \"text\": \"$payload\", \"icon_emoji\": \":gem:\"}" "$WEBHOOK_URL"
+curl -X POST --data-urlencode "payload={\"channel\": \"#treasure_hunting\", \"username\": \"Happy Treasure Hunters\", \"text\": \"$payload\", \"icon_emoji\": \":gem:\"}" "$WEBHOOK_URL"
 
 echo
 
-# Uh oh are those pirates??
+# # Uh oh are those pirates??
 echo "Uh oh..."
 payload="I just lost it..."
 sleep 5
-curl -X POST --data-urlencode "payload={\"channel\": \"#test-notifications\", \"username\": \"Sad Treasure Hunters\", \"text\": \"$payload\", \"icon_emoji\": \":pirate_flag:\"}" "$WEBHOOK_URL"
+curl -X POST --data-urlencode "payload={\"channel\": \"#treasure_hunting\", \"username\": \"Sad Treasure Hunters\", \"text\": \"$payload\", \"icon_emoji\": \":pirate_flag:\"}" "$WEBHOOK_URL"
 echo
 
 # Notify Teams
-# html_payload=$(echo $payload | sed -i -r 's/.*/&<br>/' )
+html_payload=$(echo $payload | sed -i -r 's/.*/&<br>/' )
 
-# curl --write-out %{http_code} -H "Content-Type: application/json" -d "{'title': 'I found something important', 'text':'$html_payload'}" "$WEBHOOK_URL"
+curl --write-out %{http_code} -H "Content-Type: application/json" -d "{'title': 'I found something important', 'text':'$html_payload'}" "$WEBHOOK_URL"
